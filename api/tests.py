a0ac883111c2ffa0cc1af1043ce1a0394f143d9f
liam-armstrong <liam.armstrong124@gmail.com>
2019-01-20 14:56:57 -0800

    added unit test for purchasing at model level

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from api.models import Product
 
-# Create your tests here.
+class ProductTestCase(TestCase):
+    def setUp(self):
+        Product.objects.create(title="Producta",price=12.50, inventory_count=50)
+        Product.objects.create(title="Productb",price=70, inventory_count=0)
+    def test_purchaseInStock(self):
+        # purchasing a product in stock should reduce it's inventory count by 1
+        a = Product.objects.get(title="Producta")
+        a.purchase()
+        self.assertEqual(49, a.inventory_count)
+    def test_purchaseNotInStock(self):
+        # purchasing a product not in stock should raise a ValueError
+        b = Product.objects.get(title="Productb")
+        flag = False
+        try:
+            b.purchase()
+        except ValueError:
+            flag = True
+        self.assertTrue(flag)
+        
